7013fc92eba0924b138efb05c1951ecda7300c37
dennis647 <dennis.brokking@gmail.com>
2023-12-04 16:14:52 +0100

    Added a simple financial application code, starting to add scanner

---
+++ readme.md
@@ -0,0 +1,1 @@
+Kandidatnr: 2022
--- src/main/java/org/kristiania/FinanceApp.java
+++ src/main/java/org/kristiania/FinanceApp.java
@@ -1,6 +1,8 @@
 package org.kristiania;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 class Income {
     private double workIncome;
@@ -21,7 +23,16 @@ class Expense {
     private double amount;
 
     public Expense(String category, double amount){
+        this.category = category;
+        this.amount = amount;
+    }
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
     }
 }
 
@@ -29,7 +40,20 @@ class Expenses {
     private Map<String, Double> categories;
 
     public Expenses() {
+        categories = new HashMap<>();
+    }
 
+    public void addExpense(String category, double amount) {
+        Expense expense = new Expense(category, amount);
+        categories.put(category, categories.getOrDefault(category, 0.0) + amount);
+    }
+
+    public Map<String, Double> getCategories() {
+        return categories;
+    }
+
+    public double getTotalExpenses() {
+        return categories.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 }
 
@@ -38,13 +62,32 @@ class Savings {
     private double currentAmountSaved;
 
     public Savings(double savingsGoal) {
+        this.savingsGoal = savingsGoal;
+        this.currentAmountSaved = 0.0;
+    }
 
+    public double getCurrentAmountSaved() {
+        return currentAmountSaved;
+    }
+
+    public double getSavingsGoal() {
+        return savingsGoal;
+    }
+
+    public void addToSavings(double amount) {
+        currentAmountSaved += amount;
+    }
+
+    public int timeToReachGoal(double monthlySaved) {
+        if (monthlySaved <= 0) return -1;
+        double remaining = savingsGoal - currentAmountSaved;
+        return (int) Math.ceil(remaining / monthlySaved);
     }
 }
 
 
 public class FinanceApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
     }
 }
